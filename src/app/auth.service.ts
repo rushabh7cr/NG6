c3f0f668abe6d379dbe6ec4f339fcc18319c0d9e
rs <shahrushabh199@gmail.com>
2018-07-05 01:13:46 +0530

    server js updated

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
       name
     })
     .subscribe(data=>{
-      console.log("zzzzzzz");
+      
       console.log(data);
     },err=>{
       console.log(err);
--- src/server.js
+++ src/server.js
@@ -1,18 +1,49 @@
 let express             = require("express"),
     bodyParser          = require("body-parser"),
     app                 = express();
-
+    fs                  = require("fs");
 
 app.use(bodyParser.urlencoded({ extended: false }))    
 app.use(bodyParser.json());
 
 app.post("/api",function(req,res){
     let name =req.body.name;
-    console.log(req);
-    
-    console.log(name);
-    res.send({name}); 
-})
+   // console.log(req);
+  // res.send({name}); 
+    function splitbywords(data){
+        var words = data.split(/\s+/);
+        return words;  
+    }
+
+    function wordmap(words){
+        var map={}
+        words.forEach(function(key){
+            if(map.hasOwnProperty(key)){
+                words[key]++;
+            }else{
+                words[key]=1;
+            }
+        });
+        return words;
+    }
+
+
+    fs.readFile('../R.txt','utf-8',function(err,data){
+        if(err){
+            console.log(err);
+            }
+        var words = splitbywords(data);
+        var map   = wordmap(words);
+       
+        res.send(words);
+        console.log(map)
+       
+    });
+   // console.log(name);
+   
+});
+
+
 app.get("/test",function(req,res){
     res.json({time:new Date()})
 })
